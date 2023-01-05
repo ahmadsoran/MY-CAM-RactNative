77f71a5d19d3e2b45e3fa15c2b813690ddff7ccd
ahmadsoran <ahmadsorann@gmail.com>
2023-01-06 01:53:39 +0300

    refactor

--- App.tsx
+++ App.tsx
@@ -1,35 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useEffect, useState } from "react";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import { GetFromStorage } from "./hooks/AsyncStorage";
-import OnboardingScreen from "./screens/onboarding/Screen";
-import { ShowOnboardingStore } from "./States/onboarding/ShowOnboarding";
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const showOnboarding = ShowOnboardingStore((state) => state.show);
-  const setShowOnboarding = ShowOnboardingStore((state) => state.setShow);
 
-  useEffect(() => {
-    GetFromStorage("Onboard-completed").then((res) => {
-      if (res) {
-        setShowOnboarding(false);
-      }
-    });
-  }, []);
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        {showOnboarding ? (
-          <OnboardingScreen />
-        ) : (
-          <Navigation colorScheme={colorScheme} />
-        )}
+        <Navigation colorScheme={colorScheme} />
         <StatusBar animated={true} style="inverted" />
       </SafeAreaProvider>
     );
