6b2d3e5552fd8af6285a56ca2a2f673428862244
ahmadsoran <ahmadsorann@gmail.com>
2023-01-05 14:16:56 +0300

    add user data type

---
+++ @types/UserRegisterInput.ts
@@ -0,0 +1,10 @@
+export interface UserRegisterInfo {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  city: string;
+  gendar: string;
+  brd: string;
+}
