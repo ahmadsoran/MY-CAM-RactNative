4a270b45000e2cc9b05567cc976a12feaeadaa1c
ahmadsoran <ahmadsorann@gmail.com>
2023-01-07 01:17:40 +0300

    init

---
+++ @types/UserCredintal.ts
@@ -0,0 +1,15 @@
+export interface UserRegisterInfo {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  city: string;
+  gendar: string;
+  brd: string;
+}
+
+export interface UserSginInInfo {
+  username: string;
+  password: string;
+}
